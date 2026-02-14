45ff1c40fb9da2ef7774d31ec06783e47510cd57
pavantkalyan1 <pavankalyanexmail@gmail.com>
2026-02-14 20:26:02 +0530

    Added tests

--- src/test/java/com/Automation/Framework/Tests.java
+++ src/test/java/com/Automation/Framework/Tests.java
@@ -1,5 +1,12 @@
 package com.Automation.Framework;
 
-public class Tests {
-    
+import org.testng.annotations.Test;
+
+public class Tests extends Base {
+
+    @Test
+    public void openSite(){
+        // `driver` is initialized in Base.Setup()
+        System.out.println("Page title: " + driver.getTitle());
+    }
 }
