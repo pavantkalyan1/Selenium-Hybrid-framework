204bf5b4e725f3b345b7eece439048aface373e0
pavantkalyan1 <pavankalyanexmail@gmail.com>
2026-02-14 20:03:26 +0530

    Added Base class & page objects
    Added Base class & page objects

--- src/main/java/com/Automation/Framework/Base.java
+++ src/main/java/com/Automation/Framework/Base.java
@@ -1,7 +1,50 @@
 package com.Automation.Framework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import com.Automation.Framework.utils.Utilities.ConfigReader;
+import java.time.Duration;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 public class Base {
 
-    private WebDriver driver;
+    protected WebDriver driver;  //1.driver declaration
+    protected ConfigReader config; 
     
+   @BeforeMethod
+    public void Setup(){
+    //2.Browser Initialisation
+        config = new ConfigReader();  // 3.Initialising configReader class
+        String browser = config.getbrowser();
+
+        if(browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        }
+        else {
+            throw new RuntimeException("Browser initialisation issue"+ browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(config.getURL());
+  
+    }
+
+    @AfterMethod
+    public void teardown()
+    {
+        if(driver!= null){
+            driver.quit();
+        }
+    }
+
+    protected Logger log = LogManager.getLogger(Base.class); //5.Logging setup
 }
