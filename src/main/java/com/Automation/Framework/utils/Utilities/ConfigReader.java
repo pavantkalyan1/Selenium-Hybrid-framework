3e76bc39be1c673dad64ca851657308bf9cbe176
pavantkalyan1 <pavankalyanexmail@gmail.com>
2026-02-14 16:00:15 +0530

    basic fix
    basic fix

--- src/main/java/com/Automation/Framework/utils/Utilities/ConfigReader.java
+++ src/main/java/com/Automation/Framework/utils/Utilities/ConfigReader.java
@@ -1,4 +1,4 @@
-package com.Automation.Framework.utils;
+package com.Automation.Framework.utils.Utilities;
 
 import java.util.Properties;
 import java.io.FileInputStream;
@@ -11,19 +11,19 @@ public class ConfigReader {
 
         try{
             prop = new Properties();
-            FileinputStream fis = new FileInputStream("src/main/Resources/Config.properties");
+            FileInputStream fis = new FileInputStream("src/main/Resources/Config.properties");
             prop.load(fis);
         }
         catch(Exception e){
-            throw new RuntimeException("Failed to load config file!");
+            throw new RuntimeException("Failed to load config file!", e);
         }
     }
 
     public String getbrowser(){
-        return prop.getProperty(browser);
+        return prop.getProperty("browser");
     }
 
     public String getURL(){
-        return prop.getProperty(URL);
+        return prop.getProperty("URL");
     }
 }
